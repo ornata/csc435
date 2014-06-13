0af67d1c8808d0cf4f860095fd6a63486b5d2163
Nicolas Guillemot <nlguillemot@gmail.com>
2014-06-13 08:08:05 -0700

    work-in-progress const/field/method parsing

--- a2/CbTopLevelVisitor.cs
+++ a2/CbTopLevelVisitor.cs
@@ -6,6 +6,7 @@ namespace FrontEnd {
 
 public class TopLevelVisitor : Visitor {
     private bool InUsingList = false;
+    private CbClass CurrentClass = null;
 
     public override void Visit(AST_kary node, object data) {
         if (node.Tag == NodeType.UsingList) {
@@ -45,8 +46,76 @@ public class TopLevelVisitor : Visitor {
         }
     }
 
+    private static CbType gettype(AST ast) {
+        // TODO
+        return null;
+    }
+
     public override void Visit(AST_nonleaf node, object data) {
-        Console.WriteLine("blah3");
+        if (node.Tag == NodeType.Class) {
+            NameSpace top = NameSpace.TopLevelNames;
+            // create class descriptor
+            CbClass parent = null;
+            if (node[1] != null) {
+                parent = (CbClass) top.LookUp((node[1] as AST_leaf).Sval);
+            }
+
+            CbClass clazz = new CbClass((node[0] as AST_leaf).Sval, parent);
+
+            // add it to top-level namespace and check for conflicts
+            if (top.AddMember(clazz) == false) {
+                Console.WriteLine("Error: Multiple definitions for class {0} " +
+                                  "(subsequent definition at {1}",
+                                  clazz.Name, node.LineNumber);
+            }
+
+            CurrentClass = clazz;
+        }
+        else if (node.Tag == NodeType.Const) {
+            string name = (node[1] as AST_leaf).Sval;
+            CbType type = gettype(node[0]);
+            CbConst c = new CbConst(name, type);
+
+            if (CurrentClass.AddMember(c) == false) {
+                Console.WriteLine("Error: Multiple member declarations of {0} in class {1} " +
+                                  "(subsequent definition at {2}",
+                                  c.Name,
+                                  CurrentClass.Name,
+                                  node.LineNumber);
+            }
+        }
+        else if (node.Tag == NodeType.Field) {
+            // TODO: iterate over all names in the IdentList
+            string name = "temp";
+            CbType type = gettype(node[0]);
+            CbField f = new CbField(name, type);
+
+            if (CurrentClass.AddMember(f) == false) {
+                Console.WriteLine("Error: Multiple member declarations of {0} in class {1} " +
+                                  "(subsequent definition at {2}",
+                                  f.Name,
+                                  CurrentClass.Name,
+                                  node.LineNumber);
+            }
+        }
+        else if (node.Tag == NodeType.Method) {
+            // TODO: Get all info
+            string name = "temp";
+            bool isStatic = false;
+            CbType rt = null;
+            IList<CbType> argType = null;
+
+            CbMethod m = new CbMethod(name, isStatic, rt, argType);
+
+            if (CurrentClass.AddMember(m) == false) {
+                Console.WriteLine("Error: Multiple member declarations of {0} in class {1} " +
+                                  "(subsequent definition at {2}",
+                                  m.Name,
+                                  CurrentClass.Name,
+                                  node.LineNumber);
+            }
+        }
+
         for (int i = 0; i < node.NumChildren; i++) {
             if (node[i] != null) {
                 node[i].Accept(this, i);
