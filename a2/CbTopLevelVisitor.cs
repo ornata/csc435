97ae79363c19b3e3b83b43ddf3811f60200d02fb
Nicolas Guillemot <nlguillemot@gmail.com>
2014-06-13 06:18:00 -0700

    placeholder AST traversal

--- a2/CbTopLevelVisitor.cs
+++ a2/CbTopLevelVisitor.cs
@@ -5,16 +5,26 @@ using System.Collections.Generic;
 namespace FrontEnd {
 
 public class TopLevelVisitor : Visitor {
-	public override void Visit(AST_kary node, object data) {
-		Console.WriteLine("blah1");
+    public override void Visit(AST_kary node, object data) {
+        Console.WriteLine("blah1");
+        for (int i = 0; i < node.NumChildren; i++) {
+            if (node[i] != null) {
+                node[i].Accept(this, i);
+            }
+        }
     }
 
-	public override void Visit(AST_leaf node, object data) {
-		Console.WriteLine("blah2");
+    public override void Visit(AST_leaf node, object data) {
+        Console.WriteLine("blah2");
     }
 
-	public override void Visit(AST_nonleaf node, object data) {
-		Console.WriteLine("blah3");
+    public override void Visit(AST_nonleaf node, object data) {
+        Console.WriteLine("blah3");
+        for (int i = 0; i < node.NumChildren; i++) {
+            if (node[i] != null) {
+                node[i].Accept(this, i);
+            }
+        }
     }
 }
 
