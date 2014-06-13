8f2d0f3871c446dd9fa44037b53f6f0d3e370ed1
Nicolas Guillemot <nlguillemot@gmail.com>
2014-06-13 06:51:34 -0700

    import System namespace into top-level namespace

--- a2/CbTopLevelVisitor.cs
+++ a2/CbTopLevelVisitor.cs
@@ -5,17 +5,44 @@ using System.Collections.Generic;
 namespace FrontEnd {
 
 public class TopLevelVisitor : Visitor {
+    private bool InUsingList = false;
+
     public override void Visit(AST_kary node, object data) {
+        if (node.Tag == NodeType.UsingList) {
+            InUsingList = true;
+        }
+
         Console.WriteLine("blah1");
         for (int i = 0; i < node.NumChildren; i++) {
             if (node[i] != null) {
                 node[i].Accept(this, i);
             }
         }
+
+        if (node.Tag == NodeType.UsingList) {
+            InUsingList = false;
+        }
     }
 
     public override void Visit(AST_leaf node, object data) {
-        Console.WriteLine("blah2");
+        if (InUsingList) {
+            if (node.Sval == "System") {
+                NameSpace top = NameSpace.TopLevelNames;
+                NameSpace sys = (NameSpace) top.LookUp("System");
+
+                CbClass obj = (CbClass) sys.LookUp("Object");
+                top.AddMember(obj);
+
+                CbClass st = (CbClass) sys.LookUp("String");
+                top.AddMember(st);
+                
+                CbClass console = (CbClass) sys.LookUp("Console");
+                top.AddMember(console);
+
+                CbClass i32 = (CbClass) sys.LookUp("Int32");
+                top.AddMember(i32);
+            }
+        }
     }
 
     public override void Visit(AST_nonleaf node, object data) {
