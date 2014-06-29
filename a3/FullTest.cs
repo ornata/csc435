ef45357293a68c2acd948e276420d14e765093ef
Nicolas Guillemot <nlguillemot@gmail.com>
2014-06-28 17:54:26 -0700

    added FullTest

---
+++ a3/FullTest.cs
@@ -0,0 +1,34 @@
+// test of everything, but without a using clause
+
+class Foo {
+    public const int theAnswer = 42;
+    public const string hiThere = "hello";
+    public const char itsAnX = 'x';
+
+    public int a;
+    public string b;
+    public char c;
+
+    public static void Main() {
+        Foo f;
+        f = new Bar();
+        int r;
+        r = f.Umm(3,4);
+    }
+
+    public virtual int Ummm( int a, int b ) {
+        System.Console.WriteLine("This is Foo");
+        return a+b;
+    }
+
+}
+
+
+class Bar : Foo {
+    public int x;
+
+    public override int Umm( int aa, int bb ) {
+        System.Console.WriteLine("This is Bar");
+        return a-b;
+    }
+}
