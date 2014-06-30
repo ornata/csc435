fa69001cc0d5aed015c1a68db6df3e922987c915
Jessica Paquette <jlpaquet@uvic.ca>
2014-06-30 00:07:41 -0700

    boolean type checking

--- a3/CbSemanticCheckVisitor.cs
+++ a3/CbSemanticCheckVisitor.cs
@@ -327,7 +327,7 @@ public class SemanticCheckVisitor: Visitor {
             node[0].Accept(this,data);
             node[1].Accept(this,data);
 
-            if (isIntegerType(node[0].Type) && isIntegerType(node[1].Type) {
+            if (isIntegerType(node[0].Type) && isIntegerType(node[1].Type)) {
                 node.Type = CbType.Bool;
             }
 
@@ -342,9 +342,18 @@ public class SemanticCheckVisitor: Visitor {
         case NodeType.Or:
             node[0].Accept(this,data);
             node[1].Accept(this,data);
-            /* TODO ... check types */
-            node.Type = CbType.Bool;
+
+                if (node[0].Type == CbType.Bool && node[1].Type == CbType.Bool) {
+                    node.Type = CbType.Bool;
+                }
+
+                else {
+                    Start.SemanticError(node[1].LineNumber, "boolean operations can only be performed on boolean operands");
+                    node.Type = CbType.Error;
+                }
+
             break;
+
         default:
             throw new Exception(String.Format("Line {0} Unexpected tag: {1}",
                                 node.LineNumber, node.Tag));
